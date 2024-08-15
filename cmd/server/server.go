c513e094e662fefeeb2e8918bb0eade08ff386a9
aserafim <correia.alefe@gmail.com>
2024-08-14 21:13:10 -0300

    Criada funcionalidade para criar novas categorias

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,24 +1,36 @@
 package main
 
 import (
+	"database/sql"
 	"graphql_go/graph"
+	"graphql_go/internal/database"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatal("failed to open database: %v", err)
+	}
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
+	defer db.Close()
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	categoryDB := database.NewCategory(db)
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		CategoryDB: categoryDB,
+	}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
